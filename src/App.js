90502fbe773931296787eee17d9f14fecb517bae
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-04-12 14:11:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <p>This is a react component1.</p>
+      </div>
+      <div>
+        <p>This is a react component2.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Person.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Person.css'
+const Person = () => {
+  return (
+    <div className='person'>
+        <p>Name: Harsh Sharma</p>
+        <p>Age:26</p>
+        <p>Location: Mathura</p>
+    </div>
+  )
+}   
+
+export default Person
